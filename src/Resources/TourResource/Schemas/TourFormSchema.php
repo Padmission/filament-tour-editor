70b466628df54bb013d19b5a5faba9dee4cd2466
Mark Chaney <macscr@macscr.com>
2026-03-25 22:26:19 -0400

    Use outlined style for selected tour target

--- src/Resources/TourResource/Schemas/TourFormSchema.php
+++ src/Resources/TourResource/Schemas/TourFormSchema.php
@@ -64,7 +64,8 @@ class TourFormSchema
                     Actions::make([
                         Action::make('pickElement')
                             ->label(fn (Get $get): string => filled($get('element')) ? 'Change target' : 'Pick target')
-                            ->color(fn (Get $get): string => filled($get('element')) ? 'gray' : 'primary')
+                            ->color('primary')
+                            ->outlined(fn (Get $get): bool => filled($get('element')))
                             ->icon('heroicon-o-cursor-arrow-rays')
                             ->extraAttributes(fn (Component $component): array => [
                                 'data-tour-pick-target' => 'true',
